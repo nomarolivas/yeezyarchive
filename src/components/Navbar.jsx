1c615d6cf0a5f0ac0f150a5217e670e58d0738aa
Nomar Olivas <nomarolivas@gmail.com>
2025-07-07 18:06:27 -0700

    Added Navbar dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,20 +4,55 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
 
-    const [nav, setNav]= useState(false)
+    const [isOpen, setIsOpen] = useState(false)
 
-    const handleNav = () => {
-        setNav(!nav)
-    }
+
 
     return (
-        <div>
-            
-            <Link  className= "link" to="/">Home</Link>
-            
 
 
+      <div className='bg-slate-500 h-[100px]  mx-auto flex justify-between items-center'>
+    <div className="relative">
+      
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="p-2 flex flex-col space-y-1 z-50"
+      >
+        <span className="w-6 h-0.5 bg-black"></span>
+        <span className="w-6 h-0.5 bg-black"></span>
+        <span className="w-6 h-0.5 bg-black"></span>
+      </button>
+
+   
+      {isOpen && (
+        <div className="absolute top-12 left-0 w-40 bg-white shadow-lg rounded-lg p-2 space-y-2">
+          <a
+            href="#"
+            className="block px-4 py-2 text-gray-700 hover:bg-gray-100 rounded"
+            onClick={() => setIsOpen(false)}
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className="block px-4 py-2 text-gray-700 hover:bg-gray-100 rounded"
+            onClick={() => setIsOpen(false)}
+          >
+            Seasons
+          </a>
+          <a
+            href="#"
+            className="block px-4 py-2 text-gray-700 hover:bg-gray-100 rounded"
+            onClick={() => setIsOpen(false)}
+          >
+            Info
+          </a>
         </div>
+      )}
+    </div>
+
+    </div>
+  
         
     )
 }
