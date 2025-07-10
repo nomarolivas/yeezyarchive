b9ee0dcac1154561ea8bae30cbd26ba0d7b0ecc7
Nomar Olivas <nomarolivas@gmail.com>
2025-07-09 22:06:01 -0700

    Adding Seasons Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,19 +36,34 @@ const Navbar = () => {
           >
             Home
           </a>
-          <a
+          {/* <a
             href="#"
             className="block px-4 py-2 text-gray-700 hover:bg-gray-100 rounded"
             onClick={() => setIsOpen(false)}
           >
             Seasons
-          </a>
+
+            
+          </a> */}
+
+          <button type="button" class="inline-flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900" aria-expanded="false">
+    <span className="block px-4 py-2 text-gray-700 hover:bg-gray-100 rounded">Seasons</span>
+    <svg class="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+      <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+    </svg>
+  </button>
+
+
+
+           
+          
+
           <a
             href="#"
             className="block px-4 py-2 text-gray-700 hover:bg-gray-100 rounded"
             onClick={() => setIsOpen(false)}
           >
-            Info
+            About
           </a>
         </div>
       )}
@@ -66,3 +81,5 @@ const Navbar = () => {
 }
 
 export default Navbar;
+
+<script src="https://unpkg.com/@themesberg/flowbite@1.1.1/dist/flowbite.bundle.js"></script>
